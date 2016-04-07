e0cec282f2889b96491615eff349a8763a343e9d
Kevin Saff <kevin.saff@gmail.com>
2016-04-07 10:23:35 -0500

    I guess I can't rely on such platform-specific float rounding.

--- tests/test_projections.py
+++ tests/test_projections.py
@@ -3,6 +3,11 @@ from fractalprojection.projections import MercatorProjection
 
 
 class TestMercatorProjection(unittest.TestCase):
+    def assertCollectionsAlmostEqual(self, first, second, places=None):
+        self.assertEqual(len(first), len(second), msg='Collection lengths differ {} != {}'.format(first, second))
+        for i, (a, b) in enumerate(zip(first, second)):
+            self.assertAlmostEqual(a, b, places, msg='Collection elements @{} differ {} != {}'.format(i, first, second))
+
     def test_bounds(self):
         proj = MercatorProjection((-80, +80), (-180, +180))
 
@@ -11,7 +16,7 @@ class TestMercatorProjection(unittest.TestCase):
         self.assertEqual(proj.get((+80, +180)), (1, 1))
         self.assertEqual(proj.get((-80, +180)), (0, 1))
 
-        self.assertEqual(proj.get((0, 0)), (0.5, 0.5))
+        self.assertCollectionsAlmostEqual(proj.get((0, 0)), (0.5, 0.5))
 
 
     def test_bbox(self):
